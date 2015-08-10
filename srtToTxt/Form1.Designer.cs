a14840aa330c8ceef64dbbc02a56c1a1278a0b97
evangelalessandro <evangel.alessandro@gmail.com>
2015-08-10 12:49:47 +0300

    Primo

---
+++ srtToTxt/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace srtToTxt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNameFile = new System.Windows.Forms.TextBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNameFile
+            // 
+            this.txtNameFile.AllowDrop = true;
+            this.txtNameFile.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtNameFile.Location = new System.Drawing.Point(12, 12);
+            this.txtNameFile.Name = "txtNameFile";
+            this.txtNameFile.Size = new System.Drawing.Size(406, 20);
+            this.txtNameFile.TabIndex = 0;
+            this.txtNameFile.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.txtNameFile.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSelectFile.Location = new System.Drawing.Point(424, 12);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSelectFile.TabIndex = 1;
+            this.btnSelectFile.Text = "Select srt file";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnExport.Location = new System.Drawing.Point(373, 41);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(126, 63);
+            this.btnExport.TabIndex = 2;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 116);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.txtNameFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.OnDragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNameFile;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button btnExport;
+    }
+}
+
---
+++ srtToTxt/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace srtToTxt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            EnableExport();
+            txtNameFile.TextChanged += TxtNameFile_TextChanged;
+            btnExport.Click += BtnExport_Click;
+        }
+
+        private void BtnExport_Click(object sender, EventArgs e)
+        {
+            EsportaFile();
+        }
+
+        private void EsportaFile()
+        {
+            StringBuilder fileContent = new StringBuilder();
+            var srtContent = File.ReadAllLines(txtNameFile.Text);
+
+            var i = 0;
+            foreach (var item in srtContent)
+            {
+                i++;
+                if (i == 3)
+                {
+                    fileContent.Append(item.Trim() + " ");
+                }
+                else if (i == 4)
+                {
+                    i = 0;
+                }
+            }
+            var openFileDialog1 = new SaveFileDialog();
+
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+            openFileDialog1.FileName = "Output_" + DateTime.Now.ToString("yyyy_MM_dd hh_mm_ss") + ".txt";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                File.WriteAllText(openFileDialog1.FileName, fileContent.ToString());
+
+                try
+                {
+                    Process.Start(openFileDialog1.FileName);
+                }
+                catch (Exception)
+                {
+                }
+            }
+
+        }
+
+        private void TxtNameFile_TextChanged(object sender, EventArgs e)
+        {
+            EnableExport();
+        }
+
+        private void EnableExport()
+        {
+            btnExport.Enabled = (txtNameFile.Text.Length > 0);
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            var openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.Filter = "srt files (*.srt)|*.srt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtNameFile.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNameFile.ReadOnly = true;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string filename;
+            var validData = GetFilename(out filename, e);
+            txtNameFile.Text = filename;
+        }
+
+        private void OnDragEnter(object sender, System.Windows.Forms.DragEventArgs e)
+        {
+            string filename;
+            var validData = GetFilename(out filename, e);
+            if (validData)
+            {
+
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        protected bool GetFilename(out string filename, DragEventArgs e)
+        {
+            filename = String.Empty;
+
+            if ((e.AllowedEffect & DragDropEffects.Copy) == DragDropEffects.Copy)
+            {
+                Array data = ((IDataObject)e.Data).GetData("FileName") as Array;
+                if (data != null)
+                {
+                    if ((data.Length == 1) && (data.GetValue(0) is String))
+                    {
+                        filename = ((string[])data)[0];
+                        string ext = Path.GetExtension(filename).ToLower();
+                        if ((ext == ".srt"))
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
